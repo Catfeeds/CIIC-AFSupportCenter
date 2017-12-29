b2dd0eb1e8f494a109e341b7df6a1a70d58eb1d3
zhaogang <zhaogang@ciicsh.com>
2017-12-29 13:58:28 +0800

    提交补充医疗理赔部分代码

---
+++ HealthMedical/InsuranceCommandService/InsuranceCommandService-Business/src/main/java/com/ciicsh/gto/afsupportcenter/healthmedical/commandservice/business/SupplyMedicalDossierCommandService.java
@@ -0,0 +1,16 @@
+package com.ciicsh.gto.afsupportcenter.healthmedical.commandservice.business;
+
+import com.ciicsh.gto.afsupportcenter.healthmedical.commandservice.entity.po.SupplyMedicalDossierPO;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ * 补充医疗案卷表 服务类
+ * </p>
+ *
+ * @author zhaogang
+ * @since 2017-12-29
+ */
+public interface SupplyMedicalDossierCommandService extends IService<SupplyMedicalDossierPO> {
+	
+}
---
+++ HealthMedical/InsuranceCommandService/InsuranceCommandService-Dao/src/main/java/com/ciicsh/gto/afsupportcenter/healthmedical/commandservice/dao/SupplyMedicalInvoiceMapper.java
@@ -0,0 +1,16 @@
+package com.ciicsh.gto.afsupportcenter.healthmedical.commandservice.dao;
+
+import com.ciicsh.gto.afsupportcenter.healthmedical.commandservice.entity.po.SupplyMedicalInvoicePO;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+  * 补充医疗受理单相关发票表 Mapper 接口
+ * </p>
+ *
+ * @author zhaogang
+ * @since 2017-12-29
+ */
+public interface SupplyMedicalInvoiceMapper extends BaseMapper<SupplyMedicalInvoicePO> {
+
+}
