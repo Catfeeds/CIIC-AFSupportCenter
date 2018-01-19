0160f41619c3525d52b05d9ff7b1f3a7974fd071
houwanhua <houwanhua@ciicsh.com>
2018-01-19 18:01:01 +0800

    update

--- FlexibleBenefit/FBQueryService/FBQueryService-Host/src/test/java/com/ciicsh/gto/afsupportcenter/flexiblebenefit/fbqueryservice/service/GiftQueryServiceTest.java
+++ FlexibleBenefit/FBQueryService/FBQueryService-Host/src/test/java/com/ciicsh/gto/afsupportcenter/flexiblebenefit/fbqueryservice/service/GiftQueryServiceTest.java
@@ -1,29 +1,29 @@
-package com.ciicsh.gto.afsupportcenter.flexiblebenefit.fbqueryservice.service;
-
-import com.baomidou.mybatisplus.plugins.Page;
-import com.ciicsh.gto.afsupportcenter.flexiblebenefit.entity.po.GiftPO;
-import com.ciicsh.gto.afsupportcenter.flexiblebenefit.fbqueryservice.FlexibleQueryApplication;
-import com.ciicsh.gto.afsupportcenter.flexiblebenefit.fbqueryservice.business.GiftQueryService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = FlexibleQueryApplication.class)
-public class GiftQueryServiceTest {
-
-    @Autowired
-    private GiftQueryService giftQueryService;
-
-    @Test
-    public void selectByIdTest() {
-        Page<GiftPO> page = new Page<>(1, 5);
-        GiftPO entity = new GiftPO();
-        entity.setStatus(1);
-        page = giftQueryService.queryGiftList(page, entity);
-        System.out.println(page.toString());
-    }
-
-}
+//package com.ciicsh.gto.afsupportcenter.flexiblebenefit.fbqueryservice.service;
+//
+//import com.baomidou.mybatisplus.plugins.Page;
+//import com.ciicsh.gto.afsupportcenter.flexiblebenefit.entity.po.GiftPO;
+//import com.ciicsh.gto.afsupportcenter.flexiblebenefit.fbqueryservice.FlexibleQueryApplication;
+//import com.ciicsh.gto.afsupportcenter.flexiblebenefit.fbqueryservice.business.GiftQueryService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = FlexibleQueryApplication.class)
+//public class GiftQueryServiceTest {
+//
+//    @Autowired
+//    private GiftQueryService giftQueryService;
+//
+//    @Test
+//    public void selectByIdTest() {
+//        Page<GiftPO> page = new Page<>(1, 5);
+//        GiftPO entity = new GiftPO();
+//        entity.setStatus(1);
+//        page = giftQueryService.queryGiftList(page, entity);
+//        System.out.println(page.toString());
+//    }
+//
+//}
