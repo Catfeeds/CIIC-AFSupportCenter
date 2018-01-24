17d09e633a37266f59647f230c391644389ade68
zhangxj <zhangxj@ciicsh.com>
2018-01-24 20:18:57 +0800

    创建企业任务单

--- SocialSecurity/SocCommandService/SocCommandService-Api/src/main/java/com/ciicsh/gto/afsupportcenter/socialsecurity/soccommandservice/api/SsComTaskProxy.java
+++ SocialSecurity/SocCommandService/SocCommandService-Api/src/main/java/com/ciicsh/gto/afsupportcenter/socialsecurity/soccommandservice/api/SsComTaskProxy.java
@@ -2,6 +2,8 @@ package com.ciicsh.gto.afsupportcenter.socialsecurity.soccommandservice.api;
 
 import com.ciicsh.gto.afsupportcenter.socialsecurity.soccommandservice.api.dto.JsonResult;
 import com.ciicsh.gto.afsupportcenter.socialsecurity.soccommandservice.api.dto.SsComAccountParamDto;
+import com.ciicsh.gto.afsupportcenter.socialsecurity.soccommandservice.api.dto.SsComTaskDTO;
+import com.ciicsh.gto.commonservice.util.dto.Result;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,17 +15,15 @@ import java.util.List;
  * 企业社保账户信息查询接口
  */
 @FeignClient("service-soccommandservice")
-@RequestMapping("/api/soccommandservice/sscom")
-public interface SsComProxy {
-
+@RequestMapping("/api/soccommandservice/sscomtask")
+public interface SsComTaskProxy {
     /**
-     * 获取企业社保账户信息表
+     * 保存企业任务单
      *
-         * @param paramDto
+     * @param ssComTaskDTO
      * @return
-         */
-    @RequestMapping("/getSsComAccountList")
-    JsonResult<List<com.ciicsh.gto.afsupportcenter.socialsecurity.soccommandservice.api.dto.SsComAccountDTO>>
-    getSsComAccountList(@RequestBody SsComAccountParamDto paramDto);
+     */
+    @PostMapping("/saveSsComTask")
+    public Result saveSsComTask(@RequestBody SsComTaskDTO ssComTaskDTO);
 
 }
