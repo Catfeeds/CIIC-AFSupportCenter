f96b6a7ee326986d216319b8ee9273c4d39c5597
houwanhua <houwanhua@ciicsh.com>
2018-01-30 11:06:20 +0800

    update

--- HealthMedical/InsuranceService-Host/src/test/java/com/ciicsh/gto/afsupportcenter/healthmedical/business/SupplyMedicalAcceptanceServiceImplTest.java
+++ HealthMedical/InsuranceService-Host/src/test/java/com/ciicsh/gto/afsupportcenter/healthmedical/business/SupplyMedicalAcceptanceServiceImplTest.java
@@ -1,20 +1,20 @@
-package com.ciicsh.gto.afsupportcenter.healthmedical.business;
-
-import com.ciicsh.gto.afsupportcenter.healthmedical.InsuranceApplication;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = InsuranceApplication.class)
-public class SupplyMedicalAcceptanceServiceImplTest {
-    @Autowired
-    private SupplyMedicalAcceptanceService supplyMedicalAcceptanceService;
-
-    @Test
-    public void syncAcceptanceSummary() {
-        supplyMedicalAcceptanceService.syncAcceptanceSummaryDetail();
-    }
-}
+//package com.ciicsh.gto.afsupportcenter.healthmedical.business;
+//
+//import com.ciicsh.gto.afsupportcenter.healthmedical.InsuranceApplication;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = InsuranceApplication.class)
+//public class SupplyMedicalAcceptanceServiceImplTest {
+//    @Autowired
+//    private SupplyMedicalAcceptanceService supplyMedicalAcceptanceService;
+//
+//    @Test
+//    public void syncAcceptanceSummary() {
+//        supplyMedicalAcceptanceService.syncAcceptanceSummaryDetail();
+//    }
+//}
