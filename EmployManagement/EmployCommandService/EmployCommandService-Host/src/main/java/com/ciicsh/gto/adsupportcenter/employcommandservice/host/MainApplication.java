9c07ea09d648c7b4614eaa10876779a7f04bc7e6
zhangzhiwen <zhangzhiwen@ciicsh.com>
2018-01-30 12:27:15 +0800

    雇佣管理

--- EmployManagement/EmployCommandService/EmployCommandService-Host/src/main/java/com/ciicsh/gto/adsupportcenter/employcommandservice/host/MainApplication.java
+++ EmployManagement/EmployCommandService/EmployCommandService-Host/src/main/java/com/ciicsh/gto/adsupportcenter/employcommandservice/host/MainApplication.java
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.Import;
 @MapperScan("com.ciicsh.gto.afsupportcenter.employmanagement.employcommandservice.dao")
 @SpringBootApplication(scanBasePackages = {"com.ciicsh.gto.afsupportcenter.employmanagement.employcommandservice.business","com.ciicsh.gto.adsupportcenter.employcommandservice.host.controller"})
 @Import({CustomConfiguration.class, MybatisPlusConfig.class})
-@EnableFeignClients
+//@EnableFeignClients
 public class MainApplication{
 
     private final static Logger logger = LoggerFactory.getLogger(MainApplication.class);
