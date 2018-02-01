f2395c1c2401f8b05bfdab4ce8a85233cadf752a
guwei <guwei@ciicsh.com>
2018-02-01 19:42:31 +0800

    材料收缴 update

--- CredentialsManagement/CredentialsCommandService/CredentialsCommandService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/credentialscommandservice/host/controller/EmpCredentialsDealController.java
+++ CredentialsManagement/CredentialsCommandService/CredentialsCommandService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/credentialscommandservice/host/controller/EmpCredentialsDealController.java
@@ -50,10 +50,6 @@ public class EmpCredentialsDealController {
     @Autowired
     private MaterialTypeRelationService materialTypeRelationService;
 
-    private static final int LV1 = 1;
-    private static final int LV2 = 2;
-    private static final int LV3 = 3;
-
     /**
      * 查询任务单跟进记录
      * @param taskId
@@ -166,17 +162,21 @@ public class EmpCredentialsDealController {
             if (taskDetialDTO.getTaskId() == null) {
                 taskMaterial.setCreatedBy("gu");
                 taskMaterial.setCreatedTime(new Date());
+                taskMaterial.setModifiedBy("gu");
+                taskMaterial.setModifiedTime(new Date());
+                return JsonResult.success(taskMaterialService.insert(taskMaterial));
+            } else {
+                taskMaterial.setModifiedBy("gu");
+                taskMaterial.setModifiedTime(new Date());
+                return JsonResult.success(taskMaterialService.updateTaskMaterials(taskMaterial));
             }
-            taskMaterial.setModifiedBy("gu");
-            taskMaterial.setModifiedTime(new Date());
-            return JsonResult.success(taskMaterialService.insertOrUpdate(taskMaterial));
         } else {
             return JsonResult.faultMessage();
         }
     }
 
     /**
-     * 获取任务单材料收缴信息
+     * 获取任务单材料收缴信息{level:[materialsId],...}
      * @param taskId
      * @return
      */
@@ -184,7 +184,31 @@ public class EmpCredentialsDealController {
     public JsonResult getMaterials(@PathVariable("taskId") String taskId) {
         TaskMaterial taskMaterial = taskMaterialService.selectByTaskId(taskId);
         List<MaterialTypeRelation> materials = materialTypeRelationService.selectList(taskMaterial.getMaterialIds());
-        return JsonResult.success(materials);
+        HashMap<String, List<String>> resultMap = new HashMap<>(7);
+        List list00 = new ArrayList<>();
+        List list11 = new ArrayList<>();
+        List list12 = new ArrayList<>();
+        List list21 = new ArrayList<>();
+        List list22 = new ArrayList<>();
+        List list30 = new ArrayList<>();
+        List list40 = new ArrayList<>();
+        materials.stream().forEach(i -> {
+            if ("0-0".equals(i.getLevel())) { list00.add(i.getMaterialId());}
+            if ("1-1".equals(i.getLevel())) { list11.add(i.getMaterialId());}
+            if ("1-2".equals(i.getLevel())) { list12.add(i.getMaterialId());}
+            if ("2-1".equals(i.getLevel())) { list21.add(i.getMaterialId());}
+            if ("2-2".equals(i.getLevel())) { list22.add(i.getMaterialId());}
+            if ("3-0".equals(i.getLevel())) { list30.add(i.getMaterialId());}
+            if ("4-0".equals(i.getLevel())) { list40.add(i.getMaterialId());}
+        });
+        resultMap.put("main",list00);
+        resultMap.put("dh",list11);
+        resultMap.put("zh",list12);
+        resultMap.put("marryWithoutChild",list21);
+        resultMap.put("marryWithChild",list22);
+        resultMap.put("remarry",list30);
+        resultMap.put("settle",list40);
+        return JsonResult.success(resultMap);
     }
 
     /**
