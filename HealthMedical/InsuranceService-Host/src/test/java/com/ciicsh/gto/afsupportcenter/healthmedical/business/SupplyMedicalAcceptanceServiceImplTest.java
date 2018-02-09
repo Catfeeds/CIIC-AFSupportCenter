c5166f77f987dd89db6bfd7972e2b615a1ec8ee9
houwanhua <houwanhua@ciicsh.com>
2018-02-09 13:42:03 +0800

    update

--- HealthMedical/InsuranceService-Host/src/test/java/com/ciicsh/gto/afsupportcenter/healthmedical/business/SupplyMedicalAcceptanceServiceImplTest.java
+++ HealthMedical/InsuranceService-Host/src/test/java/com/ciicsh/gto/afsupportcenter/healthmedical/business/SupplyMedicalAcceptanceServiceImplTest.java
@@ -1,36 +1,36 @@
-package com.ciicsh.gto.afsupportcenter.healthmedical.business;
-
-import com.ciicsh.gto.afsupportcenter.healthmedical.InsuranceApplication;
-import com.ciicsh.gto.afsupportcenter.healthmedical.entity.po.SupplyMedicalAcceptance;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.io.IOException;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = InsuranceApplication.class)
-public class SupplyMedicalAcceptanceServiceImplTest {
-    @Autowired
-    private SupplyMedicalAcceptanceService supplyMedicalAcceptanceService;
-
-    @Test
-    public void syncAcceptanceSummary() {
-        supplyMedicalAcceptanceService.syncAcceptanceSummaryDetail();
-    }
-
-    @Test
-    public void importAcceptanceXls() {
-//        supplyMedicalAcceptanceService.importAcceptanceXls();
-    }
-
-    @Test
-    public void test() {
-        SupplyMedicalAcceptance supplyMedicalAcceptance = supplyMedicalAcceptanceService.selectById("201801150415-2");
-        if (supplyMedicalAcceptance != null) {
-            System.out.println(supplyMedicalAcceptance.toString());
-        }
-    }
-}
+//package com.ciicsh.gto.afsupportcenter.healthmedical.business;
+//
+//import com.ciicsh.gto.afsupportcenter.healthmedical.InsuranceApplication;
+//import com.ciicsh.gto.afsupportcenter.healthmedical.entity.po.SupplyMedicalAcceptance;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.io.IOException;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = InsuranceApplication.class)
+//public class SupplyMedicalAcceptanceServiceImplTest {
+//    @Autowired
+//    private SupplyMedicalAcceptanceService supplyMedicalAcceptanceService;
+//
+//    @Test
+//    public void syncAcceptanceSummary() {
+//        supplyMedicalAcceptanceService.syncAcceptanceSummaryDetail();
+//    }
+//
+//    @Test
+//    public void importAcceptanceXls() {
+////        supplyMedicalAcceptanceService.importAcceptanceXls();
+//    }
+//
+//    @Test
+//    public void test() {
+//        SupplyMedicalAcceptance supplyMedicalAcceptance = supplyMedicalAcceptanceService.selectById("201801150415-2");
+//        if (supplyMedicalAcceptance != null) {
+//            System.out.println(supplyMedicalAcceptance.toString());
+//        }
+//    }
+//}
