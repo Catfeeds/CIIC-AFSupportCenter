8f0abdef54226abc4181ec69126003abd7a0e160
zhangxj <zhangxj@ciicsh.com>
2018-02-11 15:27:22 +0800

    业务类型修改

--- HouseFund/FundSiteService/FundSiteService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/housefund/siteservice/messageBus/TaskSink.java
+++ HouseFund/FundSiteService/FundSiteService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/housefund/siteservice/messageBus/TaskSink.java
@@ -13,37 +13,37 @@ public interface TaskSink {
     /**
      * 雇员新增
      */
-    String AF_EMP_IN = "common_taskservice_af_empin";
+    String AF_EMP_IN = "common_taskservice_af_empin_channel";
 
     /**
      * 雇员终止
      */
-    String AF_EMP_OUT = "common_taskservice_af_empout";
+    String AF_EMP_OUT = "common_taskservice_af_empout_channel";
 
     /**
      * 雇员补缴
      */
-    String AF_EMP_MAKE_UP = "common_taskservice_af_emp_make_up";
+    String AF_EMP_MAKE_UP = "common_taskservice_af_emp_make_up_channel";
 
     /**
      * 雇员翻牌
      */
-    String AF_EMP_COMPANY_CHANGE = "common_taskservice_af_emp_company_change";
+    String AF_EMP_COMPANY_CHANGE = "common_taskservice_af_emp_company_change_channel";
 
     /**
      * 雇员服务协议调整
      */
-    String AF_EMP_AGREEMENT_ADJUST = "common_taskservice_af_emp_agreement_adjust";
+    String AF_EMP_AGREEMENT_ADJUST = "common_taskservice_af_emp_agreement_adjust_channel";
 
     /**
      * 雇员服务协议更正
      */
-    String AF_EMP_AGREEMENT_UPDATE = "common_taskservice_af_emp_agreement_update";
+    String AF_EMP_AGREEMENT_UPDATE = "common_taskservice_af_emp_agreement_update_channel";
 
     /**
      * 财务中心付款申请返回
      */
-    String PAY_APPLY_PAY_STATUS_STREAM = "pay-apply-pay-status-stream";
+    String PAY_APPLY_PAY_STATUS_STREAM = "pay-apply-pay-status-stream_channel";
 
     /**
      * 公积金办理
