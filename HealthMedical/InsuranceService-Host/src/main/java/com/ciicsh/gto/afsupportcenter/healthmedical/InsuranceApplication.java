68e96e03917a9344a87e0b4d8802b3d7917d8183
zhaogang <zhaogang@ciicsh.com>
2018-02-22 21:35:05 +0800

    处理前道任务单相关代码

--- HealthMedical/InsuranceService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/healthmedical/InsuranceApplication.java
+++ HealthMedical/InsuranceService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/healthmedical/InsuranceApplication.java
@@ -4,12 +4,20 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * SpringBoot 方式启动类
  *
  * @author xiweizhen
  */
+@EnableFeignClients({"com.ciicsh.gto.sheetservice.api",
+    "com.ciicsh.gto.settlementcenter.invoicecommandservice.api",
+    "com.ciicsh.gto.basicdataservice.api",
+    "com.ciicsh.gto.afcompanycenter.queryservice.api",
+    "com.ciicsh.gto.afcompanycenter.commandservice.api",
+    "com.ciicsh.gto.employeecenter.apiservice.api"
+})
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan(basePackages = {"com.ciicsh.gto.afsupportcenter.healthmedical.dao"})
