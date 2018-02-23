db3a71b615a3294fba1c6305114b7f88ad2029b5
xiweizhen <xiweizhen@ciicsh.com>
2018-02-23 10:32:21 +0800

    合并分支

--- HealthMedical/InsuranceService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/healthmedical/InsuranceApplication.java
+++ HealthMedical/InsuranceService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/healthmedical/InsuranceApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * SpringBoot 方式启动类
