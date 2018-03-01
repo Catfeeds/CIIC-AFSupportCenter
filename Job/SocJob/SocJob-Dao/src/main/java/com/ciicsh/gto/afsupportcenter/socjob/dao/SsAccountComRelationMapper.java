dd670a59dcbc73e16d94b03a6e13825c84db2346
houwanhua <houwanhua@ciicsh.com>
2018-03-01 10:20:16 +0800

    update

--- Job/SocJob/SocJob-Dao/src/main/java/com/ciicsh/gto/afsupportcenter/socjob/dao/SsAccountComRelationMapper.java
+++ Job/SocJob/SocJob-Dao/src/main/java/com/ciicsh/gto/afsupportcenter/socjob/dao/SsAccountComRelationMapper.java
@@ -4,11 +4,7 @@ package com.ciicsh.gto.afsupportcenter.socjob.dao;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.ciicsh.gto.afsupportcenter.socjob.entity.SsAccountComRelation;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * <p>
