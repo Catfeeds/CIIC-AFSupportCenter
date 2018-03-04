50457199986ac3525a1440f12097ba0fe013c4c2
yujiawei <yujiawei@ciicsh.com>
2018-03-04 17:30:54 +0800

    新增getComFundAccountDetail接口

--- HouseFund/FundService/FundService-Dao/src/main/java/com/ciicsh/gto/afsupportcenter/housefund/fundservice/dao/HfComAccountMapper.java
+++ HouseFund/FundService/FundService-Dao/src/main/java/com/ciicsh/gto/afsupportcenter/housefund/fundservice/dao/HfComAccountMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.ciicsh.gto.afsupportcenter.housefund.fundservice.bo.customer.ComAccountExtBo;
 import com.ciicsh.gto.afsupportcenter.housefund.fundservice.bo.customer.ComAccountParamExtBo;
 import com.ciicsh.gto.afsupportcenter.housefund.fundservice.bo.customer.ComAccountTransBo;
+import com.ciicsh.gto.afsupportcenter.housefund.fundservice.entity.ComFundAccountDetailPO;
 import com.ciicsh.gto.afsupportcenter.housefund.fundservice.entity.ComFundAccountPO;
 import com.ciicsh.gto.afsupportcenter.housefund.fundservice.entity.HfComAccount;
 import org.apache.ibatis.annotations.Param;
@@ -34,6 +35,9 @@ public interface HfComAccountMapper extends BaseMapper<HfComAccount> {
                                                  @Param("hfType") Byte hfType,
                                                  @Param("comHfMonth") String comHfMonth,
                                                  @Param("accountNumber") String accountNumber);
+
+
+    ComFundAccountDetailPO getComFundAccountDetail(@Param("comAccountId") int comAccountId,@Param("hfType") Byte hfType);
 
     /**
      * 查询企业公积金账户信息
