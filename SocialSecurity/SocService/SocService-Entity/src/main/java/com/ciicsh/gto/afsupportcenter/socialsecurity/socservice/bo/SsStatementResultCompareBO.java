98936b740687adf7eb1b1752b46b0698cd813d99
wengxk <wengxiaokum@ciicsh.com>
2018-03-14 20:06:00 +0800

    社保对账功能修改

--- SocialSecurity/SocService/SocService-Entity/src/main/java/com/ciicsh/gto/afsupportcenter/socialsecurity/socservice/bo/SsStatementResultCompareBO.java
+++ SocialSecurity/SocService/SocService-Entity/src/main/java/com/ciicsh/gto/afsupportcenter/socialsecurity/socservice/bo/SsStatementResultCompareBO.java
@@ -9,4 +9,28 @@ public class SsStatementResultCompareBO {
     private String employeeId;
     private String employeeName;
     private Integer diffHeadcount;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public Integer getDiffHeadcount() {
+        return diffHeadcount;
+    }
+
+    public void setDiffHeadcount(Integer diffHeadcount) {
+        this.diffHeadcount = diffHeadcount;
+    }
 }
