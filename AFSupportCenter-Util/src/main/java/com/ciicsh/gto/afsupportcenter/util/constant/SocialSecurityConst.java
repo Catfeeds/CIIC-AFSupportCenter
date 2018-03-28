d55125cbbc92b796fca3f8846ca8c13cd3443758
likai <likai@ciicsh.com>
2018-03-28 18:53:07 +0800

    雇员公积金任务单，类型仅参照task_category（同社保做法保持一致）

--- AFSupportCenter-Util/src/main/java/com/ciicsh/gto/afsupportcenter/util/constant/SocialSecurityConst.java
+++ AFSupportCenter-Util/src/main/java/com/ciicsh/gto/afsupportcenter/util/constant/SocialSecurityConst.java
@@ -19,7 +19,7 @@ public interface SocialSecurityConst {
     public static final String FUND_OUT_UNIT_KEY = "FundOutUnit";
     public static final String REMIT_WAY_KEY = "RemitWay";
     public static final String PAYMENT_TYPE_KEY = "PaymentType";
-    public static final String HF_TASK_CATEGORY_KEY = "HFTaskCategory";
+//    public static final String HF_TASK_CATEGORY_KEY = "HFTaskCategory";
 
     //结算区县
     public static final Map<String, String> DISTRICT_MAP = new HashMap<String, String>() {
@@ -64,18 +64,18 @@ public interface SocialSecurityConst {
         }
     };
 
-    public static final Map<String, String> HF_TASK_CATEGORY_MAP = new HashMap<String, String>() {
-        {
-            put("1", "新开");
-            put("2", "转入");
-            put("3", "启封");
-            put("4", "转出");
-            put("5", "封存");
-            put("6", "补缴");
-            put("7", "调整");
-            put("8", "转移");
-        }
-    };
+//    public static final Map<String, String> HF_TASK_CATEGORY_MAP = new HashMap<String, String>() {
+//        {
+//            put("1", "新开");
+//            put("2", "转入");
+//            put("3", "启封");
+//            put("4", "转出");
+//            put("5", "封存");
+//            put("6", "补缴");
+//            put("7", "调整");
+//            put("8", "转移");
+//        }
+//    };
 
     /**
      * 未处理
