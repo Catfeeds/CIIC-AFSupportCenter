7533cdcdcdcbec1eafd31253e3ecacef4c8baac0
linhui <linhui@ciicsh.com>
2018-04-01 13:04:39 +0800

    公积金支付

--- HouseFund/FundService/SiteService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/housefund/siteservice/host/controller/HfFundPayController.java
+++ HouseFund/FundService/SiteService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/housefund/siteservice/host/controller/HfFundPayController.java
@@ -15,6 +15,7 @@ import com.ciicsh.gto.afsupportcenter.util.web.response.JsonResultKit;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
