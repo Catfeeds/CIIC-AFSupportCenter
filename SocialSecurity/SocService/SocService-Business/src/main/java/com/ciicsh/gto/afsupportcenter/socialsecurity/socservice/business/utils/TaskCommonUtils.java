e02817914d173e8a3b52561390f715e85f807dac
likai <likai@ciicsh.com>
2018-04-02 09:07:54 +0800

    公积金任务单接收，及回调更新

--- SocialSecurity/SocService/SocService-Business/src/main/java/com/ciicsh/gto/afsupportcenter/socialsecurity/socservice/business/utils/TaskCommonUtils.java
+++ SocialSecurity/SocService/SocService-Business/src/main/java/com/ciicsh/gto/afsupportcenter/socialsecurity/socservice/business/utils/TaskCommonUtils.java
@@ -122,7 +122,7 @@ public class TaskCommonUtils {
         List<AfEmpSocialUpdateDateDTO> paramsList = new ArrayList<>();
 
         if(StringUtils.isBlank(ssEmpTaskBO.getBusinessInterfaceId())){
-            throw new BusinessException("messionId 为空.");
+            throw new BusinessException("missionId 为空.");
         }
 
         if(ssEmpTaskBO.getListEmpBasePeriod()==null){//说明是逆向调整，没有福利段
@@ -212,13 +212,13 @@ public class TaskCommonUtils {
 
                     // 以下为调整后的费用段回调处理：
                     // 如果oldAgreementId存在，且是转出或封存时，说明是调整非0转0
-                    if (org.apache.commons.lang.StringUtils.isNotEmpty(ssEmpTaskBO.getOldAgreementId()) && (
+                    if (StringUtils.isNotEmpty(ssEmpTaskBO.getOldAgreementId()) && (
                         ssEmpTaskBO.getTaskCategory().equals(Integer.parseInt(SocialSecurityConst.TASK_TYPE_5))
                             || ssEmpTaskBO.getTaskCategory().equals(Integer.parseInt(SocialSecurityConst.TASK_TYPE_6))
                     )) {
                         // 此时需要回调一个只有开始确认时间的，金额为0的费用段
                         afEmpSocialUpdateDateDTO = new AfEmpSocialUpdateDateDTO();
-                        afEmpSocialUpdateDateDTO.setEmpAgreementId(Long.valueOf(ssEmpTaskBO.getBusinessInterfaceId())); //messionId
+                        afEmpSocialUpdateDateDTO.setEmpAgreementId(Long.valueOf(ssEmpTaskBO.getBusinessInterfaceId())); //missionId
                         afEmpSocialUpdateDateDTO.setCompanyId(ssEmpTaskBO.getCompanyId());//企业Id
                         afEmpSocialUpdateDateDTO.setItemCode(ssEmpBaseDetail.getSsType());//社保险种
                         afEmpSocialUpdateDateDTO.setCompanyConfirmAmount(BigDecimal.ZERO);
@@ -231,7 +231,7 @@ public class TaskCommonUtils {
                     } else {
                         // 通常的处理方式
                         afEmpSocialUpdateDateDTO = new AfEmpSocialUpdateDateDTO();
-                        afEmpSocialUpdateDateDTO.setEmpAgreementId(Long.valueOf(ssEmpTaskBO.getBusinessInterfaceId())); //messionId
+                        afEmpSocialUpdateDateDTO.setEmpAgreementId(Long.valueOf(ssEmpTaskBO.getBusinessInterfaceId())); //missionId
                         afEmpSocialUpdateDateDTO.setCompanyId(ssEmpTaskBO.getCompanyId());//企业Id
                         afEmpSocialUpdateDateDTO.setItemCode(ssEmpBaseDetail.getSsType());//社保险种
                         afEmpSocialUpdateDateDTO.setCompanyConfirmAmount(ssEmpBaseDetail.getComAmount());
