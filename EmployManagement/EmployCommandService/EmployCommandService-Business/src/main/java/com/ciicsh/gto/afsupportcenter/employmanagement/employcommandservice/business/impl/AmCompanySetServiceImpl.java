024bc100b691a5db2a6db5825041f33277ba3471
zhangzhiwen <zhangzhiwen@ciicsh.com>
2018-04-26 13:17:42 +0800

    退工邮寄信息

--- EmployManagement/EmployCommandService/EmployCommandService-Business/src/main/java/com/ciicsh/gto/afsupportcenter/employmanagement/employcommandservice/business/impl/AmCompanySetServiceImpl.java
+++ EmployManagement/EmployCommandService/EmployCommandService-Business/src/main/java/com/ciicsh/gto/afsupportcenter/employmanagement/employcommandservice/business/impl/AmCompanySetServiceImpl.java
@@ -1,12 +1,17 @@
 package com.ciicsh.gto.afsupportcenter.employmanagement.employcommandservice.business.impl;
 
+import com.ciicsh.gto.afsupportcenter.employmanagement.employcommandservice.api.dto.CompanyDTO;
+import com.ciicsh.gto.afsupportcenter.employmanagement.employcommandservice.api.dto.CompanyParamDTO;
 import com.ciicsh.gto.afsupportcenter.employmanagement.employcommandservice.bo.AmCompanySetBO;
+import com.ciicsh.gto.afsupportcenter.employmanagement.employcommandservice.business.utils.ReasonUtil;
 import com.ciicsh.gto.afsupportcenter.employmanagement.employcommandservice.entity.AmCompanySet;
 import com.ciicsh.gto.afsupportcenter.employmanagement.employcommandservice.dao.AmCompanySetMapper;
 import com.ciicsh.gto.afsupportcenter.employmanagement.employcommandservice.business.IAmCompanySetService;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.ciicsh.gto.afsupportcenter.util.StringUtil;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -29,5 +34,55 @@ public class AmCompanySetServiceImpl extends ServiceImpl<AmCompanySetMapper, AmC
             return  list.get(0);
         }
         return null;
+    }
+
+    @Override
+    public CompanyDTO queryCompanyDTO(CompanyParamDTO companyParamDTO) {
+        List<CompanyDTO> list = baseMapper.queryCompanyDTO(companyParamDTO);
+        if(null!=list&&list.size()>0){
+            return  list.get(0);
+        }
+        return  null;
+    }
+
+    @Override
+    public boolean saveCompanyDTO(CompanyDTO companyDTO) {
+        AmCompanySetBO amCompanySetBO = new AmCompanySetBO();
+        amCompanySetBO.setCompanyId(companyDTO.getCompanyId());
+        List<AmCompanySetBO> list = baseMapper.queryAmCompanySet(amCompanySetBO);
+        AmCompanySet amCompanySet = new AmCompanySet();
+        if(null!=list&&list.size()>0){
+            amCompanySetBO = list.get(0);
+            org.springframework.beans.BeanUtils.copyProperties(amCompanySetBO,amCompanySet);
+        }
+
+        amCompanySet.setCompanyId(companyDTO.getCompanyId());
+        if(companyDTO.getMailContinue()){
+            amCompanySet.setPhone(companyDTO.getPhone());
+            amCompanySet.setMailContinue(companyDTO.getMailContinue());
+            amCompanySet.setMailAdress(companyDTO.getMailAdress());
+            if(StringUtil.isEmpty(companyDTO.getPostCode())){
+                amCompanySet.setPostCode("  ");
+            }else{
+                amCompanySet.setPostCode(companyDTO.getPostCode());
+            }
+            amCompanySet.setRecipient(companyDTO.getRecipient());
+        }else{
+            amCompanySet.setMailContinue(companyDTO.getMailContinue());
+            amCompanySet.setPhone(" ");
+            amCompanySet.setMailAdress(" ");
+            amCompanySet.setPostCode(" ");
+            amCompanySet.setRecipient(" ");
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        amCompanySet.setCreatedTime(now);
+        amCompanySet.setModifiedTime(now);
+        amCompanySet.setCreatedBy(ReasonUtil.getUserId());
+        amCompanySet.setModifiedBy(ReasonUtil.getUserId());
+        amCompanySet.setActive(true);
+
+        return this.insertOrUpdate(amCompanySet);
+
     }
 }
