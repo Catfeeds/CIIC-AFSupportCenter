2f0dfa133f3136633563f013afb6a7099a6dc61a
linhui <linhui@ciicsh.com>
2018-05-08 15:40:12 +0800

    提高公积金job的查询时间

--- HouseFund/FundService/FundService-Entity/src/main/java/com/ciicsh/gto/afsupportcenter/housefund/fundservice/bo/payment/HfPrintRemittedBookBO.java
+++ HouseFund/FundService/FundService-Entity/src/main/java/com/ciicsh/gto/afsupportcenter/housefund/fundservice/bo/payment/HfPrintRemittedBookBO.java
@@ -8,20 +8,25 @@ import java.math.BigDecimal;
 public class HfPrintRemittedBookBO {
     private String comAccountName;
     private String hfComAccount;
+    private String paymentYear;
     private String paymentMonth;
     private String curdate;
+    private Integer curYear;
+    private Integer curMonth;
+    private Integer curDay;
     private String bankName;
-    private BigDecimal remittedAmountAdd;
-    private BigDecimal remittedAmountReduce;
-    private BigDecimal remittedAmount;
-    private BigDecimal remittedCountEmpAdd;
-    private BigDecimal remittedCountEmpReduce;
-    private Integer remittedCountEmp;
-    private BigDecimal remittedAmountLast;
-    private BigDecimal remittedCountEmpLast;
-    private BigDecimal repairAmount;
-    private Integer repairCountEmp;
+    private BigDecimal remittedAmountAdd;//本月增加金额
+    private BigDecimal remittedAmountReduce;//本月减少金额
+    private BigDecimal remittedAmount;//本月汇缴金额
+    private BigDecimal remittedCountEmpAdd;//本月增加人数
+    private BigDecimal remittedCountEmpReduce;//本月减少人数
+    private Integer remittedCountEmp;//本月汇缴人数
+    private BigDecimal remittedAmountLast;//上月汇缴金额
+    private BigDecimal remittedCountEmpLast;//上月汇缴人数
+    private BigDecimal repairAmount;//补缴金额
+    private Integer repairCountEmp;//补缴人数
     private Boolean isRemitted;//打钩汇缴
     private Boolean isRepair;//打钩补缴
-
+    private Integer pageNum;
+    private String moneyCN;
 }
