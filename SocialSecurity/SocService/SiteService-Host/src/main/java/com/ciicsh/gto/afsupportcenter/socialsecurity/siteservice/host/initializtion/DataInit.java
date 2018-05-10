3f4d00bd1cda22d1c3a53a6c7acd7b313018b8e2
zhengj <zhengj@ciicsh.com>
2018-05-10 15:55:12 +0800

    证件管理雇员新增bug修改

---
+++ SocialSecurity/SocService/SiteService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/socialsecurity/siteservice/host/initializtion/DataInit.java
@@ -0,0 +1,20 @@
+package com.ciicsh.gto.afsupportcenter.socialsecurity.siteservice.host.initializtion;/**
+ * Created by zhengj on 2018/5/9
+ */
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: zhengj
+ * @date: 2018/5/9 21:12
+ **/
+@Component
+public class DataInit implements CommandLineRunner{
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(111);
+    }
+}
