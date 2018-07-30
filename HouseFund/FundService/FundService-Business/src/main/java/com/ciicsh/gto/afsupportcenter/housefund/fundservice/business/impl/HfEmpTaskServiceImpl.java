55a8f79f2b16a08029d39b25360e43b714d02233
likai <likai@ciicsh.com>
2018-07-30 11:46:10 +0800

    GT1-5411 GT1-5412 GT1-5423

--- HouseFund/FundService/FundService-Business/src/main/java/com/ciicsh/gto/afsupportcenter/housefund/fundservice/business/impl/HfEmpTaskServiceImpl.java
+++ HouseFund/FundService/FundService-Business/src/main/java/com/ciicsh/gto/afsupportcenter/housefund/fundservice/business/impl/HfEmpTaskServiceImpl.java
@@ -158,34 +158,32 @@ public class HfEmpTaskServiceImpl extends ServiceImpl<HfEmpTaskMapper, HfEmpTask
                     if(arr[i].indexOf("desc")>0||arr[i].indexOf("asc")>0){
                         orderParam.add(arr[i]);
                     }else {
-//                        if(arr[i].indexOf("hf_account_type")!=-1)
-//                        {
-//                            String str[] = arr[i].split(" ");
-//                            String regexp = "\'";
-//                            String status = str[2].replaceAll(regexp, "");
-//                            hfEmpTaskBo.setHfAccountType(Integer.parseInt(status));
-//                        }
-//                        if(arr[i].indexOf("payment_bank")!=-1)
-//                        {
-//                            String str[] = arr[i].split(" ");
-//                            String regexp = "\'";
-//                            String status = str[2].replaceAll(regexp, "");
-//                            hfEmpTaskBo.setPaymentBank(Integer.parseInt(status));
-//                        }
-//                        if(arr[i].indexOf("hf_com_account")!=-1)
-//                        {
-//                            String str[] = arr[i].split(" ");
-//                            String regexp = "\'";
-//                            String status = str[2].replaceAll(regexp, "");
-//                            hfEmpTaskBo.setHfComAccount(status);
-//                        }
+                        if (arr[i].indexOf("desc") > 0 || arr[i].indexOf("asc") > 0) {
+                            orderParam.add(arr[i]);
+                        } else {
+                            if (arr[i].indexOf("hf_account_type") != -1) {
+                                String str[] = arr[i].split(" ");
+                                String regexp = "\'";
+                                String status = str[2].replaceAll(regexp, "");
+                                hfEmpTaskBo.setHfAccountType(Integer.parseInt(status));
+                            }
+                            if (arr[i].indexOf("payment_bank") != -1) {
+                                String str[] = arr[i].split(" ");
+                                String regexp = "\'";
+                                String status = str[2].replaceAll(regexp, "");
+                                hfEmpTaskBo.setPaymentBank(Integer.parseInt(status));
+                            }
+                            if (arr[i].indexOf("hf_com_account") != -1) {
+                                String str[] = arr[i].split(" ");
+                                String regexp = "\'";
+                                String status = str[2].replaceAll(regexp, "");
+                                hfEmpTaskBo.setHfComAccount(status);
+                            }
 
-
-                        param.add(arr[i]);
+                            param.add(arr[i]);
+                        }
                     }
-
                 }
-
             }
         }
 
