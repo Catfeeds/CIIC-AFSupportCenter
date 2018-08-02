c348487dab778fed8e8a87c273a42f168c9bde20
liyuelong <liyuelong@ciicsh.com>
2018-08-02 19:31:51 +0800

    工具类修改流

--- EmployManagement/EmployService/SiteService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/employmanagement/sitservice/host/util/WordUtils.java
+++ EmployManagement/EmployService/SiteService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/employmanagement/sitservice/host/util/WordUtils.java
@@ -4,12 +4,13 @@ import com.ciicsh.gto.afsupportcenter.util.StringUtil;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.ResourceUtils;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.URLEncoder;
 import java.util.Date;
 import java.util.Map;
@@ -22,12 +23,9 @@ public class WordUtils {
         configuration = new Configuration();
         configuration.setDefaultEncoding("utf-8");
         try {
-//            File file = ResourceUtils.getFile("classpath:template");
 
             // 以上方式不行 只能在本地拿到模板  发布到linux上去 打成jar包 要以这种方式读取模板
             ClassPathResource classPathResource = new ClassPathResource("template");
-
-//            classPathResource.getInputStream();
 
             File file = classPathResource.getFile();
 
@@ -42,14 +40,9 @@ public class WordUtils {
     }
 
     public static void exportMillCertificateWord(HttpServletRequest request, HttpServletResponse response, Map map,String title,String ftlFile) throws IOException {
-        File file = null;
-        InputStream fin = null;
-        ServletOutputStream out = null;
+
         try {
             Template freemarkerTemplate = configuration.getTemplate(ftlFile);
-            // 调用工具类的createDoc方法生成Word文档
-            file = createDoc(map,freemarkerTemplate);
-            fin = new FileInputStream(file);
 
             response.setCharacterEncoding("utf-8");
             response.setContentType("application/msword");
@@ -57,37 +50,13 @@ public class WordUtils {
             String fileName = title+ StringUtil.getDateString(new Date()) + ".doc";
             response.setHeader("Content-Disposition", "attachment;filename="
                 .concat(String.valueOf(URLEncoder.encode(fileName, "UTF-8"))));
-
-            out = response.getOutputStream();
-            byte[] buffer = new byte[512];  // 缓冲区
-            int bytesToRead = -1;
-            // 通过循环将读入的Word文件的内容输出到浏览器中
-            while((bytesToRead = fin.read(buffer)) != -1) {
-                out.write(buffer, 0, bytesToRead);
-            }
+            Writer w = new OutputStreamWriter(response.getOutputStream(), "utf-8");
+            freemarkerTemplate.process(map,w);
+            w.close();
         }catch (Exception ex) {
             ex.printStackTrace();
             throw new RuntimeException(ex);
-        }finally {
-            if(fin != null) fin.close();
-            if(out != null) out.close();
-            if(file != null) file.delete(); // 删除临时文件
         }
     }
 
-    private static File createDoc(Map<?, ?> dataMap, Template template) {
-        String name =  "sellPlan.doc";
-        File f = new File(name);
-        Template t = template;
-        try {
-            // 这个地方不能使用FileWriter因为需要指定编码类型否则生成的Word文档会因为有无法识别的编码而无法打开
-            Writer w = new OutputStreamWriter(new FileOutputStream(f), "utf-8");
-            t.process(dataMap, w);
-            w.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            throw new RuntimeException(ex);
-        }
-        return f;
-    }
 }
