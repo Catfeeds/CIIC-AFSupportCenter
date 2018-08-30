3a676ede34acb04e844ea2bfbdca359d7fe80527
linhui <linhui@ciicsh.com>
2018-08-30 15:21:50 +0800

    v1.2.1

--- SocialSecurity/SocService/SocService-Business/src/main/java/com/ciicsh/gto/afsupportcenter/socialsecurity/socservice/business/impl/SsComAccountServiceImpl.java
+++ SocialSecurity/SocService/SocService-Business/src/main/java/com/ciicsh/gto/afsupportcenter/socialsecurity/socservice/business/impl/SsComAccountServiceImpl.java
@@ -99,8 +99,11 @@ public class SsComAccountServiceImpl extends ServiceImpl<SsComAccountMapper, SsC
 
         if(ssAccount==null || ssAccount=="" ){
             condition.put("company_id",companyId);
-            SsAccountComRelation ssAccountComRelation= ssAccountComRelationMapper.selectByMap(condition).get(0);
+            SsAccountComRelation ssAccountComRelation = ssAccountComRelationMapper.selectByMap(condition).get(0);
             comAccountId = ssAccountComRelation.getComAccountId();
+            condition.put("com_account_id",comAccountId);
+            SsComAccount ssComAccount= baseMapper.selectByMap(condition).get(0);
+            ssAccount=ssComAccount.getSsAccount();
         }
         condition=new HashMap<>();
         condition.put("ss_account",ssAccount);
