8cb80302ad7d4e0a23be6346446f6713dac0a0db
liyuelong <liyuelong@ciicsh.com>
2018-09-11 09:59:50 +0800

    修改档案预增检索条件

--- EmployManagement/EmployService/EmployService-Dao/src/main/java/com/ciicsh/gto/afsupportcenter/employmanagement/employservice/dao/AmArchiveAdvanceMapper.java
+++ EmployManagement/EmployService/EmployService-Dao/src/main/java/com/ciicsh/gto/afsupportcenter/employmanagement/employservice/dao/AmArchiveAdvanceMapper.java
@@ -1,7 +1,10 @@
 package com.ciicsh.gto.afsupportcenter.employmanagement.employservice.dao;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.ciicsh.gto.afsupportcenter.employmanagement.employservice.bo.AmArchiveAdvanceBO;
 import com.ciicsh.gto.afsupportcenter.employmanagement.employservice.entity.AmArchiveAdvance;
+
+import java.util.List;
 
 /**
  * <p>
@@ -9,5 +12,5 @@ import com.ciicsh.gto.afsupportcenter.employmanagement.employservice.entity.AmAr
  * </p>
  */
 public interface AmArchiveAdvanceMapper extends BaseMapper<AmArchiveAdvance> {
-
+    List<AmArchiveAdvance> queryAdvanceList(AmArchiveAdvanceBO bo);
 }
