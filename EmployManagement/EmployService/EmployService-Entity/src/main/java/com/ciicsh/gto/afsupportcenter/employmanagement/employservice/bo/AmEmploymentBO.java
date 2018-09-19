8898e66430cb985260b1076c3f52a299f3513e16
liyuelong <liyuelong@ciicsh.com>
2018-09-19 09:46:18 +0800

    打印退工单添加公司变更

--- EmployManagement/EmployService/EmployService-Entity/src/main/java/com/ciicsh/gto/afsupportcenter/employmanagement/employservice/bo/AmEmploymentBO.java
+++ EmployManagement/EmployService/EmployService-Entity/src/main/java/com/ciicsh/gto/afsupportcenter/employmanagement/employservice/bo/AmEmploymentBO.java
@@ -179,4 +179,9 @@ public class AmEmploymentBO extends AmEmployment {
      * 是否入职
      */
     private Integer isEntry;
+
+    /**
+     * 公司变更记录
+     */
+    private List<String> companyNameList;
 }
--- EmployManagement/EmployService/SiteService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/employmanagement/sitservice/host/controller/AmArchiveTaskController.java
+++ EmployManagement/EmployService/SiteService-Host/src/main/java/com/ciicsh/gto/afsupportcenter/employmanagement/sitservice/host/controller/AmArchiveTaskController.java
@@ -21,6 +21,8 @@ import com.ciicsh.gto.afsupportcenter.util.page.PageRows;
 import com.ciicsh.gto.afsupportcenter.util.web.controller.BasicController;
 import com.ciicsh.gto.afsupportcenter.util.web.response.JsonResult;
 import com.ciicsh.gto.afsupportcenter.util.web.response.JsonResultKit;
+import com.ciicsh.gto.salecenter.apiservice.api.dto.company.CompanyNameHistoryDTO;
+import com.ciicsh.gto.salecenter.apiservice.api.proxy.CompanyNameHistoryProxy;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -59,6 +61,9 @@ public class AmArchiveTaskController extends BasicController<IAmEmploymentServic
 
     @Autowired
     private IAmEmploymentService amEmploymentService;
+
+    @Autowired
+    private CompanyNameHistoryProxy companyNameHistoryProxy;
 
     @Autowired
     private  IAmResignService amResignService;
@@ -720,6 +725,22 @@ public class AmArchiveTaskController extends BasicController<IAmEmploymentServic
     }
 
     /**
+     * 查询公司变更记录
+     * @param
+     */
+    @RequestMapping("/queryCompanyNameUpdateHistory")
+    public JsonResult<List<CompanyNameHistoryDTO>> queryCompanyNameUpdateHistory(String companyId) {
+        JsonResult<List<CompanyNameHistoryDTO>> result = new JsonResult<>();
+        com.ciicsh.gto.salecenter.apiservice.api.dto.core.JsonResult<List<CompanyNameHistoryDTO>>
+            companyListResult = companyNameHistoryProxy.getCompanyNameHistoryListByCompanyId(companyId);
+        result.setData(companyListResult.getObject());
+        if(companyListResult.getObject()==null){
+            result.setData(new ArrayList<>());
+        }
+        return result;
+    }
+
+    /**
      * 打印退工单
      * @param response
      */
@@ -737,6 +758,15 @@ public class AmArchiveTaskController extends BasicController<IAmEmploymentServic
         List<AmArchiveReturnPrintDTO> list = business.queryAmArchiveForeignerPritDate(pageInfo);
         Map<String,Object> map = new HashMap<>();
         map.put("list",list);
+        if(amEmploymentBO.getCompanyNameList() == null || amEmploymentBO.getCompanyNameList().size() == 0){
+            List<String> companys = new ArrayList<>();
+            companys.add("");
+            companys.add("");
+        }else{
+            amEmploymentBO.getCompanyNameList().add("");
+            amEmploymentBO.getCompanyNameList().add("");
+        }
+        map.put("companys",amEmploymentBO.getCompanyNameList());//原公司名称：
         try {
             WordUtils.exportMillCertificateWord(response,map,"退工单","AM_RETURN_QUADRUPLICATE_TEMP.ftl");
         } catch (Exception e) {
