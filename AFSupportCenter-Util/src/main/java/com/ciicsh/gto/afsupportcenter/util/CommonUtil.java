116ffc5fea22dff18f02989fd2ed9891a4550227
likai <likai@ciicsh.com>
2018-10-19 14:25:19 +0800

    增加重试

--- AFSupportCenter-Util/src/main/java/com/ciicsh/gto/afsupportcenter/util/CommonUtil.java
+++ AFSupportCenter-Util/src/main/java/com/ciicsh/gto/afsupportcenter/util/CommonUtil.java
@@ -1,14 +1,12 @@
 package com.ciicsh.gto.afsupportcenter.util;
 
-import com.ciicsh.gto.afsupportcenter.util.logService.LogApiUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CommonUtil {
-
     private static Logger logger = LoggerFactory.getLogger(CommonUtil.class);
 
-   public static boolean runWithRetries(int maxRetries, long millionSecs, ThrowingTask t) throws Exception {
+    public static boolean runWithRetries(int maxRetries, long millionSecs, ThrowingTask t) throws Exception {
         int count = 0;
         while (count < maxRetries) {
             try {
